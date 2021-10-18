e5ae7c7956232115da0722b0831ee0b32efe68a7
KevinKoudelka <48528358+KevinKoudelka@users.noreply.github.com>
2021-10-18 18:11:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,209 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+import * as rtdb from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
+import * as fbauth from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCa5TpRrrlL5pAWbhEqyR72GsvrrdblreY",
+    authDomain: "discord-f9823.firebaseapp.com",
+    databaseURL: "https://discord-f9823-default-rtdb.firebaseio.com",
+    projectId: "discord-f9823",
+    storageBucket: "discord-f9823.appspot.com",
+    messagingSenderId: "488378244438",
+    appId: "1:488378244438:web:6cadd89ee24e0cb2330af7",
+    measurementId: "G-NDZL27Q5GR"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+let db = rtdb.getDatabase(app);
+let titleRef = rtdb.ref(db, "/chats");
+let usersRef = rtdb.ref(db, "/users");
+let msg = document.getElementById('msg')
+let sub = document.getElementById('sub')
+let chats = document.getElementById('chats')
+let users = document.getElementById('users')
+let listChats = document.getElementById('listofChats')
+let displayname = document.getElementById('display')
+let auth = fbauth.getAuth(app);
+let useremail = ''
+let userId = ''
+let uid = ''
+let chatId = ''
+let numChats = 1;
+let currentChat = 'Chat2'
+
+let sendMsg = () => {
+  console.log(currentChat)
+  let displayRef = rtdb.ref(db, `/users/${userId}/`)
+  let channelRef = rtdb.ref(db, "/channels/" + currentChat + "/messages");
+  rtdb.onValue(displayRef, ss => {
+    rtdb.push(channelRef, {
+      msg: msg.value,
+      user: ss.val().displayName
+    })
+    msg.value = ''
+})
+}
+
+$("#login").on("click", ()=>{
+  let email = $("#logemail").val();
+  let pwd = $("#logpass").val();
+  fbauth.signInWithEmailAndPassword(auth, email, pwd).then(
+    somedata=>{
+    }).catch(function(error) {
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+});
+let renderUser = function(userObj){
+  $("#app").append(`<button type="button" id="logout">Logout</button> <button id='newChat'>New Chat</button>`);
+  $('#newChat').on("click", () => {
+    numChats += 1;
+    let chatName = "Chat" + numChats
+    let channelRef = rtdb.ref(db, "/channels/" + chatName)
+    rtdb.set(channelRef, {
+      channelName: `Chat#${numChats}`})
+  })
+  $("#logout").on("click", ()=>{
+    fbauth.signOut(auth);
+  })
+}
+let renderChats = function() {
+  $('#allChats').append(`<ul id=chats></ul> <br>
+  <input id=msg type="text" placeholder='Enter Message'> <br>
+  <button id=sub >Submit</button>`)
+}
+let renderlistUsers = function() {
+  rtdb.onValue(usersRef, ss=>{
+        let val = ss.val()
+        users.innerHTML = ''
+        for (let uid in val) {
+          let user = document.createElement('li');
+          users.appendChild(user)
+          user.innerText = val[uid].email
+        }
+        })
+}
+                     
+let renderChannels = function() {
+  let channelRef = rtdb.ref(db, `/channels/`)
+  let checkUser = rtdb.ref(db, `/users/${userId}/roles`)
+        rtdb.onValue(channelRef, ss => {
+          let val = ss.val()
+          listChats.innerHTML = ''
+          for (let channel in val) {
+            $('#listofChats').append(`<li id=${channel}>${val[channel].channelName}</li>`)
+          }
+          $("#listofChats li").click(function() {
+            currentChat = $(this).attr('id')
+            let channelRef = rtdb.ref(db, "/channels/" + currentChat)
+            rtdb.onValue(channelRef, ss=>{
+              let channel = ss.val().messages
+               $('#chats').empty();   
+              rtdb.onValue(checkUser, ss=>{
+                let role = ss.val()
+                if (role.admin == true) {
+              for (let message in channel) {
+                $("#chats").append(`<li><button class='del' data-id=${message}>X</button><button id='edit' class='edit' data-id=${message} data-message=${channel[message].msg}>Edit</button> ${channel[message].user}: ${channel[message].msg}</li> <input class='editInput' data-input=${message} />`);
+              }
+              }
+                else {
+                   let check = rtdb.ref(db, `/users/${userId}/`)
+                    for (let message in channel) {
+                      rtdb.onValue(check, ss => {
+                      if (ss.val().displayName == channel[message].user) {
+                        $("#chats").append(`<li><button class='del' data-id=${message}>X</button> <button id='edit' class='edit' data-id=${message} data-message=${channel[message].msg}>Edit</button> ${channel[message].user}: ${channel[message].msg}</li> <input class='editInput' data-input=${message} />`);
+                      }
+                      else {
+                        $("#chats").append(`<li>${channel[message].user}: ${channel[message].msg}</li>`);
+                      }
+                      })
+                      }
+           }
+            })
+            })
+            $(".del").click(function () {
+            let chatId = $(this).attr('data-id')
+            let chatRef = rtdb.ref(db, `/channels/${currentChat}/messages/${chatId}`)
+            rtdb.remove(chatRef)
+      })
+          $(".edit").click(function () {
+            let chatId = $(this).attr('data-id')
+            let input = $(this).attr('data-input')
+           $(`input[data-input=${chatId}]`).removeClass('editInput')
+            let chatRef = rtdb.ref(db, `/channels/${currentChat}/messages/${chatId}`)
+            $(`input[data-input=${chatId}]`).on("keyup", function(e) {
+                if(e.which == 13) {
+                  rtdb.update(chatRef, {
+                    msg: $(this).val()
+                  })
+                  $(`input[data-input=${chatId}]`).addClass('editInput')
+                }
+            });
+      })
+         });
+        })
+}
+fbauth.onAuthStateChanged(auth, user => {
+      if (!!user){
+        $(".login").hide();
+        $(".register").hide()
+        $("#app").show();
+        $(".background").show();
+        $(".panel").show();
+        $(".users").show();
+        useremail = user.email;
+        userId = user.uid
+        renderUser(user);
+        let channelRef = rtdb.ref(db, "/channels/" + currentChat + "/messages");
+        let checkUser = rtdb.ref(db, `/users/${userId}/roles`)
+        renderChannels()
+        chats.innerHTML = ''
+        renderlistUsers()
+      } else {
+        $(".login").show();
+        $(".register").hide()
+        $(".background").hide();
+        $(".panel").hide();
+        $(".users").hide();
+        $("#app").html("");
+        $("#register").on("click", ()=>{
+        let email = $("#regemail").val();
+        let p1 = $("#regpass1").val();
+        let p2 = $("#regpass2").val();
+        let display = $("#display").val();
+        if (p1 != p2){
+          alert("Passwords don't match");
+          return;
+        }
+        fbauth.createUserWithEmailAndPassword(auth, email, p1).then(somedata=>{
+          uid = somedata.user.uid;
+          let userRoleRef = rtdb.ref(db, `/users/${uid}/roles/user`);
+          let emailRef = rtdb.ref(db, `/users/${uid}/email`)
+          let displayRef = rtdb.ref(db, `/users/${uid}/displayName`)
+          rtdb.set(userRoleRef, true);
+          rtdb.set(emailRef, email);
+          rtdb.set(displayRef, display);
+        }).catch(function(error) {
+          var errorMessage = error.message;
+          alert(errorMessage);
+        });
+      });
+      }
+});
+
+$("#regButton").on("click", () => {
+  $(".register").toggle()
+  $(".login").toggle()
+})
+sub.addEventListener("click", sendMsg)
